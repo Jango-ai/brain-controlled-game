50efd6182412fdc5ab8412dbc697d505c4eb0f8e
Jango-ai <55050494+Jango-ai@users.noreply.github.com>
2020-03-28 12:13:56 +0100

    Codigo actualizado con logica para detectar pestañeos

--- python_event_server/lsl_reader.py
+++ python_event_server/lsl_reader.py
@@ -6,8 +6,18 @@ import random
 
 
 def process_epoch(epoch):
-    # IMPLEMENT HERE ARTIFACT DETECTION LOGIC
-    # Use send_blink_artifact_event() to send the event to the game
+    raw=epoch
+    #we keep only frontal channels
+    non_frontal_channels = ['C1', 'C2', 'C3', 'C4', 'CP1', 'CP2', 'CP3', 'CP4', 'O1', 'O2', 'Cz', 'Pz']
+    raw = raw.drop_channels(non_frontal_channels)
+    # ARTIFACT DETECTION LOGIC
+    #calculate signal max, min and range across frontal channels
+    average_max_signal=(np.amax(raw[0][0])+np.amax(raw[1][0])+np.amax(raw[2][0])+np.amax(raw[3][0]))/4
+    average_min_signal=(np.amin(raw[0][0])+np.amin(raw[1][0])+np.amin(raw[2][0])+np.amin(raw[3][0]))/4
+    signal_range = average_max_signal-average_min_signal
+    # if signal range is between 0.0002 and 0.00035 we consider it a blink 
+    if (signal_range >= 0.0002 and signal_range <= 0.00033):
+        send_blink_artifact_event() # Use send_blink_artifact_event() to send the event to the game
     return ""
 
 
@@ -40,17 +50,17 @@ def start():
     )
 
     # COMMENT OUT THIS LOOP WHEN `process_epoch` IS IMPLEMENTED
-    while True:
-        send_server_ok_signal()
-        send_blink_artifact_event()
-        time.sleep(0.3)
+    #while True:
+    #    send_server_ok_signal()
+    #    send_blink_artifact_event()
+    #    time.sleep(0.3)
 
-    # with LSLClient(info=stream_info, host=host) as client:
-    #     client_info = client.get_measurement_info()
-    #     while True:
-    #         epoch = client.get_data_as_epoch(n_samples=epoch_size_in_seconds * sfreq)
-    #         send_server_ok_signal()
-    #         process_epoch(epoch)
+     with LSLClient(info=stream_info, host=host) as client:
+         client_info = client.get_measurement_info()
+         while True:
+             epoch = client.get_data_as_epoch(n_samples=epoch_size_in_seconds * sfreq)
+             send_server_ok_signal()
+             process_epoch(epoch)
 
 
 def send_server_ok_signal():
